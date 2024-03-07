0765da9a5d436d75157fb00db38171da0e14b744
unknown <holupkern@gmail.com>
2024-03-07 14:25:58 +0800

    ported over blank qn

---
+++ 07-conditional-rendering/07-conditional-rendering/src/AirQuality.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+
+export default function AirQuality(){
+
+    const [psi, setPsi] = useState(0);
+
+    getAirQualityText = () => {
+        return "undefined";
+      }
+    
+      getAirQualityColor = () => {
+    
+      }
+    
+
+    return (
+      <React.Fragment>
+        <div>
+            <label>Enter the PSI:</label>
+            <input  type="text" 
+                    value={psi} 
+                    onChange={(e)=>{setPsi(e.target.value)}}
+            />
+        </div>
+        <h1>Air Quality</h1>
+        <div id="air-quality" 
+             style={{color:'black'}}
+        >
+            {getAirQualityText()}
+        </div>
+      </React.Fragment>
+    )
+    
+}
